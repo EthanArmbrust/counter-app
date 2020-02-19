c8b8aadf424412ad9551ac83e4aff516f5baa45a
EthanArmbrust <ethanarm@buffalo.edu>
2020-02-19 13:54:46 -0500

    Just dropped in

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,6 +7,11 @@ class Counter extends Component {
     //tags: []
   };
 
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags</p>;
 
@@ -22,10 +27,23 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+  formatCount() {
+    return this.state.count === 0 ? "Zero" : this.state.count;
   }
 }
 
